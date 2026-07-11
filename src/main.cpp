91073cc0fcbda44904f52bf8b5714aaeaa9f3afb
Watson Carawon <carawonw@gmail.com>
2026-07-11 17:01:08 -0400

    Done with chapter 4 through 'error handling'

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include <sstream>
 #include <string>
 
+bool hadError = false;
+
+void report(int line, std::string where, std::string message) {
+  std::cerr << "[line " << line << "] Error" << where << ": " << message
+            << std::endl;
+  hadError = true;
+}
+
+void error(int line, std::string message) { report(line, "", message); }
+
 void run(std::string source) {
   std::cout << "source == " << source << std::endl;
 }
@@ -22,15 +32,26 @@ void runFile(std::string arg1) {
   run(str);
 }
 
-void runPrompt() {}
+void runPrompt() {
+  while (true) {
+    std::cout << "> ";
+    std::string line{};
+    if (!std::getline(std::cin, line)) {
+      break;
+    }
+    run(line);
+    hadError = false;
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc > 2) {
     std::cerr << "Usage: lox-cpp [script]\n";
-    return 1;
+    std::exit(64);
   } else if (argc == 2) {
     runFile(std::string{argv[1]});
   } else {
+    std::cout << "Running prompt" << std::endl;
     runPrompt();
   }
 }
